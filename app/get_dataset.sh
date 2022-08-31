8c2b996018c116069dc86a9a4e8fbb53ce5951a8
Pranav-Goel <pranavgoel403@gmail.com>
2022-08-31 17:00:32 -0400

    modified get_dataset.sh script to now download all files needed for successfully running the app

--- app/get_dataset.sh
+++ app/get_dataset.sh
@@ -3,3 +3,4 @@ mkdir -p backend/data
 wget -P backend/data/ https://obj.umiacs.umd.edu/nist-data/corpus.pkl
 wget -P backend/data/ https://obj.umiacs.umd.edu/nist-data/lda_model.bin
 wget -P backend/data/ https://obj.umiacs.umd.edu/nist-data/nist_ras_documents_cleaned.csv
+wget -P backend/data/ https://obj.umiacs.umd.edu/nist-data/database.db
